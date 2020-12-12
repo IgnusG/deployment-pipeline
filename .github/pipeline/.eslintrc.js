7b214d873c77d10ac96dda06031c4ff5f8289900
IgnusG <IgnusG@users.noreply.github.com>
2020-12-13 00:14:11 +0100

    Configure typescript compilation and linting workflow

---
+++ .github/pipeline/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    extends: [
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:import/typescript",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "prettier"
+    ],
+    plugins: [
+        "@typescript-eslint",
+    ],
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json']
+    },
+    rules: {
+        "comma-dangle": [
+            "error",
+            ["arrays", "objects", "imports", "exports", "functions"].reduce((a, r) => ({ ...a, [r]: "always-multiline" }), {})
+        ],
+        "max-len": "off",
+        "import/order": ["error", {
+            alphabetize: {
+                order: 'asc',
+            },
+            "newlines-between": "always-and-inside-groups"
+        }]
+    },
+    settings: {
+        "import/resolver": {
+            typescript: {}
+        },
+    },
+};
