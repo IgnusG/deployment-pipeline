76c689baf551f597a72d11afcc8105456cd8c7ef
IgnusG <IgnusG@users.noreply.github.com>
2021-01-09 21:20:13 +0100

    Refactor deployment status function

--- .github/pipeline/src/functions/update-deployment-statuses/update-deployment-statuses.ts
+++ .github/pipeline/src/functions/update-deployment-statuses/update-deployment-statuses.ts
@@ -2,9 +2,9 @@ import { Octokit } from "@octokit/rest";
 
 import { lt } from "semver";
 
-import { queryVersion } from "functions/query-version/query-version";
-import { stringToEnvironment } from "lib/deployment/environment";
-import { AppErr, Err, flatAsyncResult, Ok, Recoverable, Result } from "lib/errors";
+import { EnvironmentNotFound, queryVersion, QueryVersionErrors } from "functions/query-version/query-version";
+import { Environment, stringToEnvironment } from "lib/deployment/environment";
+import { AppErr, Err, Ok, Recoverable, Result } from "lib/errors";
 import { GitHub } from "lib/github";
 import { Deployment, DeploymentStatus } from "lib/github/types";
 
@@ -16,28 +16,24 @@ const deploymentSuccessful = (status: DeploymentStatus, deployment: Deployment):
   return Ok(status.state === "pending" ? false : true);
 };
 
-const versionPublished = async (
-  publishedVersion: string | null | undefined,
-  version: string,
-  deployment: Deployment,
-  github: ReturnType<typeof GitHub>,
-): Promise<Result<boolean, AppErr>> => {
-  if (!publishedVersion) return Recoverable("Published Version is not available");
+const createGetLatestPublishedVersion = (): (environment: Environment) => Promise<Result<string, QueryVersionErrors>> => {
+  const publishedVersions = new Map<Environment, Result<string, QueryVersionErrors>>();
 
-  const environment = stringToEnvironment(deployment.environment);
+  return async (environment) => {
+    if (publishedVersions.has(environment))
+      return publishedVersions.get(environment) as Result<string, AppErr>;
 
-  if (publishedVersion === "") {
-    await github.removeDeployment(deployment);
+    const publishedVersion = await queryVersion(environment);
 
-    return Recoverable(`Environment ${environment} does not exist`);
-  }
-
-  return lt(publishedVersion, version) ? Ok(false) : Ok(true);
+    return publishedVersion;
+  };
 };
 
 export default async function updateDeploymentStatuses(owner: string, repo: string, octokit: Octokit): Promise<void> {
   const github = GitHub({ owner, repo }, octokit);
   const deployments = await github.listDeployments();
+
+  const getLatestPublishedVersion = createGetLatestPublishedVersion();
 
   for (const deployment of deployments) {
     const environment = stringToEnvironment(deployment.environment);
@@ -52,13 +48,24 @@ export default async function updateDeploymentStatuses(owner: string, repo: stri
       if (deploymentStatus.isErr()) return Err(deploymentStatus.error);
       if (deploymentStatus.isOk() && deploymentStatus.value) return Ok("Deployment IS already successful");
 
-      const publishedVersion = await queryVersion(environment);
-      const isVersionPublished = await flatAsyncResult(publishedVersion, (published) =>
-        versionPublished(published, version, deployment, github),
-      );
+      const publishedVersion = await getLatestPublishedVersion(environment);
 
-      if (isVersionPublished.isErr()) return Err(isVersionPublished.error);
-      if (isVersionPublished.value == false) return Ok(`Version ${version} IS already published`);
+      if (publishedVersion.isErr()) {
+        switch (publishedVersion.error.identifier) {
+          case EnvironmentNotFound.match: {
+            await github.removeDeployment(deployment);
+
+            return Ok(`Deployment for ${environment} does not exist so it was removed`);
+          }
+          default: {
+            return publishedVersion;
+          }
+        }
+      }
+
+      const isVersionPublished = lt(publishedVersion.value, version) ? false : true;
+
+      if (isVersionPublished == false) return Ok(`Version ${version} is not published yet`);
 
       await github.successfulDeployment(deployment, `Version ${version} is published`);
 
